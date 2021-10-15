13bff0178bb7adad70eff781b83da3df836c5138
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2021-10-14 21:59:49 -0300

    Finalização do Projeto Netflix

---
+++ Recriando a Interface do Netflix/js/main.js
@@ -0,0 +1,17 @@
+$('.owl-carousel, #serie').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:7
+        }
+    }
+})
+
